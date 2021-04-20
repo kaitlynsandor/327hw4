7185e8394fa4f3306f876283d8f075e1f10e497b
kaitlynsandor <kaitlyn.sandor@gmail.com>
2021-04-20 14:54:47 -0400

    updated stuff

--- player.py
+++ player.py
@@ -2,22 +2,22 @@ class Player:
     def __init__(self, type):
         self.type = type
 
-    def next_move(self, moves, board):
+    def next_move(self, moves):
         pass
 
 
 class HumanPlayer(Player):
-    def next_move(self, moves, board):
+    def next_move(self, moves):
         pass
 
 
 class RandomPlayer(Player):
-    def next_move(self, moves, board):
+    def next_move(self, moves):
         pass
 
 
 class ComputerPlayer(Player):
-    def next_move(self, moves, board):
+    def next_move(self, moves):
         pass
 
 
