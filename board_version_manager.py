466cc2ed440960cf173a649a96e2a65857755f24
kaitlynsandor <kaitlyn.sandor@gmail.com>
2021-04-23 09:30:39 -0400

    added board version manager to be implemented

---
+++ board_version_manager.py
@@ -0,0 +1,15 @@
+class Board_Version_Manager:
+    def __init__(self):
+        self.history = []
+
+    def update_history(self):
+        pass
+
+    def undo(self):
+        pass
+
+    def re_do(self):
+        pass
+
+    def next(self):
+        pass
