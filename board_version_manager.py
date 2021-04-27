7eb38ad8c6c1039bbbc735172e94ada0e002693b
flaurencyac <flaurencya.ciputra@yale.edu>
2021-04-27 11:30:55 -0400

    added board version manager classes

--- board_version_manager.py
+++ board_version_manager.py
@@ -7,13 +7,19 @@ class Board_Version_Manager: # set up the version manager
         self._history.append(state)
 
     def undo(self): # move our pointer and return a new board
-        pass
+        if self._pointer > 0:
+            self._pointer -= 1
+        else:
+            pass
 
     def re_do(self):# move our pointer and return a new board
-        pass
+        if self._pointer < len(self._history):
+            self._pointer += 1
+        else:
+            pass 
 
     def next(self):
         # chop off the part of the array we dont need
         # set pointer back to 0
-        pass
+        self._history = self._history[:self._pointer]
 
